fa85de16457eb723171b290f7930b00e072dbcd4
SemihSyntax <semih.cyln96@gmail.com>
2024-01-26 14:17:28 +0100

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "YuGiOh_TCG_DeckMaster"
+include(":app")
